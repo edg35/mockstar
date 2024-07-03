96e2134a08c4ca41c69757852e4a5babaf98bfe9
Ethan Green <66663131+edg35@users.noreply.github.com>
2024-07-03 16:00:44 -0400

    setup hero section

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -1,22 +1,69 @@
-"use client"
-import { UserButton } from "@clerk/nextjs";
+"use client";
+import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 function Navbar() {
-    const path = usePathname();
+  const path = usePathname();
+  const { user } = useUser();
+  const router = useRouter();
+
+  const handleLoginRoute = (e) => {
+    e.preventDefault();
+    router.push("/sign-in");
+  };
+
+  const handleSignupRoute = (e) => {
+    e.preventDefault();
+    router.push("/sign-up");
+  };
+
   return (
     <div className="flex p-4 items-center justify-between bg-secondary shadow-sm">
-      <Image src={'/logo.svg'} width={40} height={20} alt='logo' />
+      <Image src={"/logo.svg"} width={40} height={20} alt="logo" />
       <ul className="hidden md:flex gap-6">
-        <li className={`hover:text-primary cursor-pointer hover:font-semibold transition-all ${path=='/dashboard'&& 'text-primary font-semibold'}`}>Dashboard</li>
-        <li className={`hover:text-primary cursor-pointer hover:font-semibold transition-all ${path=='/dashboard/questions'&& 'text-primary font-semibold'}`}>Questions</li>
-        <li className={`hover:text-primary cursor-pointer hover:font-semibold transition-all ${path=='/dashboard/upgrade'&& 'text-primary font-semibold'}`}>Upgrade</li>
-        <li className={`hover:text-primary cursor-pointer hover:font-semibold transition-all ${path=='/dashboard/how'&& 'text-primary font-semibold'}`}>How it Works?</li>
+        <li
+          className={`hover:text-primary cursor-pointer hover:font-semibold transition-all ${
+            path == "/dashboard" && "text-primary font-semibold"
+          }`}
+        >
+          Dashboard
+        </li>
+        <li
+          className={`hover:text-primary cursor-pointer hover:font-semibold transition-all ${
+            path == "/dashboard/questions" && "text-primary font-semibold"
+          }`}
+        >
+          Questions
+        </li>
+        <li
+          className={`hover:text-primary cursor-pointer hover:font-semibold transition-all ${
+            path == "/dashboard/upgrade" && "text-primary font-semibold"
+          }`}
+        >
+          Upgrade
+        </li>
+        <li
+          className={`hover:text-primary cursor-pointer hover:font-semibold transition-all ${
+            path == "/dashboard/how" && "text-primary font-semibold"
+          }`}
+        >
+          How it Works?
+        </li>
       </ul>
-      <UserButton />
+      {user ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-2">
+          <Button onClick={handleSignupRoute} variant="outline">
+            Sign Up
+          </Button>
+          <Button onClick={handleLoginRoute}>Login</Button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- app/page.js
+++ app/page.js
@@ -1,8 +1,35 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      {/* Hero Section */}
+      <section className="bg-white">
+        <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+          <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
+            <Image
+              alt="careerprogresspic"
+              src={"/careerprogress.svg"}
+              height={100}
+              width={100}
+              className="absolute inset-0 h-full w-full object-fit xs:object-cover"
+            />
+          </aside>
+
+          <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+            <div className="max-w-xl lg:max-w-3xl">
+              <h1 className="mt-6 text-3xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+                Get Hired Faster with AI-Powered Job Search Tools.
+              </h1>
+
+              <p className="mt-4 leading-relaxed text-gray-500">
+                Craft a Winning Resume & Cover Letter in Minutes. Master Your
+                Interview Skills with AI Coaching.
+              </p>
+            </div>
+          </main>
+        </div>
+      </section>
+    </>
   );
 }
